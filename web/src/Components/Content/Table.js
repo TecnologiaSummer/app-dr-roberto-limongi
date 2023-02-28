189247bbebf2dae6330764bdbfb5c06773ff9b37
Leonardo <leonardo.rosa@summercomunicacao.com.br>
2023-02-28 16:32:24 -0300

    initial commit

--- web/src/Components/Content/Table.js
+++ web/src/Components/Content/Table.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import avatar from '../../Assets/avatar.png';
 
 const Table = ({ columnName, data }) => {
--- web/src/privateRoute.js
+++ web/src/privateRoute.js
@@ -1,12 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useNavigate } from  "react-router-dom";
 import { AuthContext } from "./Context/AuthContext";
 
 export const PrivateRoute = ({children}) => {
-  const { signed } = useContext(AuthContext);
+  const { signed, setUser } = useContext(AuthContext);
   const  navigate  = useNavigate()
+  function handleLogout () {
+    setUser(false)
+    navigate("/")
+  }
   return (
-    signed ? children : navigate("/")
+    signed ? children : handleLogout()
   )
 }
 
